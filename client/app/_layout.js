50c248358cd3462acac21ee9e01534a83589b59c
abdul <abdullaziz89@hotmail.com>
2024-04-14 12:16:51 +0300

    try to fix some issue

--- client/app/_layout.js
+++ client/app/_layout.js
@@ -324,20 +324,20 @@ const MyDrawer = (props) => {
                         overlayColor: "transparent",
                         drawerPosition: i18n.language === "ar" ? "right" : "left",
                     }}
-                    drawerContent={(props) =>
-                        <CustomDrawerContent
-                            {...props}
-                            user={user}
-                            setUser={setUser}
-                            isLogin={isLogin}
-                            setLogin={setLogin}
-                            getLogo={getLogo}
-                            i18n={i18n}
-                            numberViews={numberViews}
-                            setVerified={setVerified}
-                            insets={insets}
-                        />
-                    }
+                    // drawerContent={(props) =>
+                    //     <CustomDrawerContent
+                    //         {...props}
+                    //         user={user}
+                    //         setUser={setUser}
+                    //         isLogin={isLogin}
+                    //         setLogin={setLogin}
+                    //         getLogo={getLogo}
+                    //         i18n={i18n}
+                    //         numberViews={numberViews}
+                    //         setVerified={setVerified}
+                    //         insets={insets}
+                    //     />
+                    // }
                 >
                     <Drawer.Screen
                         name="(home)"
